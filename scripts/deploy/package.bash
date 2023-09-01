c75a215ac69aa3e8167735f0096e32f7b1cb794b
paulAlexSerban <paul.alex.serban@gmail.com>
2023-09-01 14:18:52 +0300

    cleanup

--- scripts/deploy/package.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# makes sure the folder containing the script will be the root folder
-cd "$(dirname "$0")" || exit
-
-echo "📦  Package"
-mkdir -p ../../package/reactjs
-cp -rfv ../../frontend/reactjs/build/* ../../package/reactjs
