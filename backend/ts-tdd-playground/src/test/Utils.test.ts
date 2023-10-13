f5373b5f6da1b7faaf4a5ec3261555ac6c614009
paulAlexSerban <paul.alex.serban@gmail.com>
2023-10-13 09:19:37 +0300

    docs: added comments

--- backend/ts-tdd-playground/src/test/Utils.test.ts
+++ backend/ts-tdd-playground/src/test/Utils.test.ts
@@ -1,8 +1,14 @@
 import { toUpperCase } from '../app/Utils';
 
 describe('toUpperCase', () => {
-    it('should convert string to uppercase', () => {
-        const result = toUpperCase('hello');
-        expect(result).toBe('HELLO');
+    it('should return upper case of a valid string', () => {
+        // Arrange
+        // - SUT = System Under Test
+        const SUT = toUpperCase;
+        const expected = 'HELLO';
+        // Act
+        const actual = SUT('hello');
+        // Assert
+        expect(actual).toEqual(expected);
     });
 });
