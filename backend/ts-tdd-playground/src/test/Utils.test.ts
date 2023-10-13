ce2d0a1971a4cd1bc9c6ae198ae7e91d18b85d3d
paulAlexSerban <paul.alex.serban@gmail.com>
2023-10-13 18:27:58 +0300

    test: added some extra tests

--- backend/ts-tdd-playground/src/test/Utils.test.ts
+++ backend/ts-tdd-playground/src/test/Utils.test.ts
@@ -12,38 +12,81 @@ describe('Utils test suite', () => {
         expect(actual).toEqual(expected);
     });
 
-    it('should return string info of a valid string', () => {
-        // Arrange
-        // - SUT = System Under Test
-        const SUT = getStringInfo;
-        const expected: stringInfo = {
+    /**
+     * @nota_bene
+     * use .toBe() for primitive types
+     * use .toEqual() for object types
+     */
+
+    describe("getStringInfo for argument 'hello'", () => {
+        test('return right length', () => {
+            // Arrange - SUT = System Under Test
+            const SUT = getStringInfo;
+            // Act
+            const actual = SUT('hello');
+            // Assert
+            expect(actual).toHaveLength(5);
+        });
+        test('return right lower case', () => {
+            // Arrange - SUT = System Under Test
+            const SUT = getStringInfo;
+            // Act
+            const actual = SUT('hello');
+            // Assert
+            expect(actual.lowerCase).toBe('hello');
+        });
+
+        test('return right upper case', () => {
+            // Arrange - SUT = System Under Test
+            const SUT = getStringInfo;
+            // Act
+            const actual = SUT('hello');
+            // Assert
+            expect(actual.upperCase).toBe('HELLO');
+        });
+
+        test('return right characters', () => {
+            // Arrange - SUT = System Under Test
+            const SUT = getStringInfo;
+            // Act
+            const actual = SUT('hello');
+            // Assert
+            expect(actual.characters).toEqual(['h', 'e', 'l', 'l', 'o']);
+        });
+
+        test('return characters contains right right character', () => {
+          // Arrange - SUT = System Under Test
+          const SUT = getStringInfo;
+          // Act
+          const actual = SUT('hello');
+          // Assert
+          expect(actual.characters).toContain<string>('h');
+      });
+        
+
+        test('return defined extraInfo', () => {
+            // Arrange - SUT = System Under Test
+            const SUT = getStringInfo;
+            // Act
+            const actual = SUT('hello');
+            // Assert
+            expect(actual.extraInfo).toEqual({});
+        });
+
+        test('return expected string info', () => {
+          // Arrange - SUT = System Under Test
+          const SUT = getStringInfo;
+          const expected: stringInfo = {
             lowerCase: 'hello',
             upperCase: 'HELLO',
             characters: ['h', 'e', 'l', 'l', 'o'],
             length: 5,
             extraInfo: {},
         };
-        // Act
-        const actual = SUT('hello');
-
-        // Assert
-        expect(actual.lowerCase).toEqual(expected.lowerCase);
-        expect(actual.upperCase).toEqual(expected.upperCase);
-
-        expect(actual.extraInfo).toEqual(expected.extraInfo);
-
-        expect(actual.characters).toHaveLength(expected.characters.length);
-
-        expect(actual.characters).toEqual(expected.characters);
-        expect(actual.characters).toContain<string>('h');
-        expect(actual.characters).toEqual(expect.arrayContaining<string>(['h', 'e', 'l', 'l', 'o']));
-
-        expect(actual.length).toEqual(expected.length);
-
-        expect(actual.extraInfo).not.toBe(undefined);
-        expect(actual.characters).not.toBeUndefined();
-        expect(actual.extraInfo).toBeDefined();
-        expect(actual.extraInfo).toBeTruthy();
-        expect(actual.characters).not.toBeNull();
+          // Act
+          const actual = SUT('hello');
+          // Assert
+          expect(actual.extraInfo).toEqual(expected.extraInfo);
+      });
     });
 });
